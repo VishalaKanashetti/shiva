274b04367eae68ca506ea0030bfde5d56640b1d7
shivraj <shivraj@TRAINEE-PC.it.com>
2017-02-17 16:54:18 +0530

    added  sop statement

---
+++ Request/src/Shivaraj/Welcome.java
@@ -0,0 +1,20 @@
+package Shivaraj;
+
+import java.io.*;  
+import javax.servlet.*;  
+import javax.servlet.http.*;  
+  
+public class Welcome extends HttpServlet {  
+  
+    public void doPost(HttpServletRequest request, HttpServletResponse response)  
+        throws ServletException, IOException {  
+  
+    response.setContentType("text/html");  
+    PrintWriter out = response.getWriter();  
+          
+    String n=request.getParameter("userName");  
+    out.print("Welcome "+n);  
+    out.print("vishala,Shivu,Anu");
+    }  
+  
+}  
